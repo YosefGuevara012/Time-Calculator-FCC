e55a2890bac97eb3a5d3aa91778d15b1844a1f99
YOSEF GUEVARA <yosef.guevara012@hotmail.com>
2022-03-01 16:09:57 -0500

    Time split function created, splits the hour, the time, and the period

--- time_calculator.py
+++ time_calculator.py
@@ -5,3 +5,19 @@ def add_time(start, duration):
 
 
     return new_time
+
+def time_split(string):
+
+  hours, minutes = string.split(":")
+
+  try:
+    minutes, period = minutes.split(" ")
+    return [hours, minutes, period]
+  
+  except:
+    minutes = minutes
+    return [hours, minutes]
+
+
+    
+
