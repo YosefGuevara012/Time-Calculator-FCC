5009c7060d0a4041412bcb35731eb9827a24605c
YOSEF GUEVARA <yosef.guevara012@hotmail.com>
2022-03-01 16:33:11 -0500

    add times function added, Initial days and hours calculation done

--- time_calculator.py
+++ time_calculator.py
@@ -1,10 +1,19 @@
 def add_time(start, duration):
 
+  hour_1, minutes_1, period = time_split(start)
+  hour_2, minutes_2         = time_split(duration)
+
+  week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+  # Calculating the Initial days and hours
+
+  hours = int(hour_1) + int(hour_2)
+  days = hours // 24
+  hours = hours % 24
 
 
 
-
-    return new_time
+  return [days, hours]
 
 def time_split(string):
 
