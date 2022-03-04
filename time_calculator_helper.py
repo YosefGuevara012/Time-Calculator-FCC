ac357df8b453a913d5bdf9034107e31f4ee3f281
YOSEF GUEVARA <yosef.guevara012@hotmail.com>
2022-03-03 20:30:55 -0500

    All functions done, the test file shows everything is working

--- time_calculator_helper.py
+++ time_calculator_helper.py
@@ -1,4 +1,4 @@
-def add_time(start, duration):
+def add_time(start, duration, day = None):
 
 
   # Spliting the start and duration times
@@ -6,7 +6,6 @@ def add_time(start, duration):
   hour_1, minutes_1, period = time_split(start)
   hour_2, minutes_2         = time_split(duration)
 
-  week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
 
   # Calculating minutes and additional hours
@@ -37,14 +36,38 @@ def add_time(start, duration):
   elif hours < 12:
     period = "AM"
 
-  # Ajusting the text result format
+
+  # Calculating the end day
+
+  week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+  if day != None:
+    day = day.capitalize()
+    index = (days % 7) + week.index(day)
+    
+
+    # Ajusting the text result format
+    if days == 0:
+      txt = "{0}:{1} {2}, {3}".format(hours,'{:02}'.format(minutes),period,week[index])
+    elif days == 1:
+      txt = "{0}:{1} {2}, {3}  (next day)".format(hours,'{:02}'.format(minutes),period, week[index])
+    elif days > 1:
+      txt = "{0}:{1} {2}, {3} ({4} days later)".format(hours,'{:02}'.format(minutes),period, week[index], days)
+
+  else:
+    
+     # Ajusting the text result format
+    if days == 0:
+      txt = "{0}:{1} {2}".format(hours,'{:02}'.format(minutes),period)
+    elif days == 1:
+      txt = "{0}:{1} {2} (next day)".format(hours,'{:02}'.format(minutes),period)
+    elif days > 1:
+      txt = "{0}:{1} {2} ({3} days later)".format(hours,'{:02}'.format(minutes),period, days)
+    
   
-  if days == 0:
-    txt = "{0}:{1} {2},".format(hours,'{:02}'.format(minutes),period)
-  elif days == 1:
-    txt = "{0}:{1} {2}, (next day)".format(hours,'{:02}'.format(minutes),period)
-  elif days > 1:
-    txt = "{0}:{1} {2}, ({3} days later)".format(hours,'{:02}'.format(minutes),period, days)
+ 
+  
+  
     
     
   
@@ -68,7 +91,7 @@ print(add_time("3:00 PM", "3:10"))
 print("********************************")
 
 print("11:30 PM,2:32")
-print(add_time("11:30 AM", "2:32"))
+print(add_time("11:30 AM", "2:32", "Monday"))
 print("********************************")
 
 print("11:30 PM,2:32")
@@ -80,7 +103,7 @@ print(add_time("10:10 PM", "3:30"))
 print("********************************")
 
 print("11:43 PM, 24:20")
-print(add_time("11:43 PM", "24:20"))
+print(add_time("11:43 PM", "24:20", "tueSday"))
 print("********************************")
 
 print("6:30 PM, 205:12")
